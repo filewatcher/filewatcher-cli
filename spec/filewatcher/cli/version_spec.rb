5403b6e34f3025bdbb3fe8719351a3c1cdc28abd
Alexander Popov <alex.wayfer@gmail.com>
2020-09-19 02:59:20 +0300

    Allow pre-release versions in version specs

--- spec/filewatcher/cli/version_spec.rb
+++ spec/filewatcher/cli/version_spec.rb
@@ -3,5 +3,5 @@
 describe 'Filewatcher::CLI::VERSION' do
   subject { Object.const_get(self.class.description) }
 
-  it { is_expected.to match(/^\d+\.\d+\.\d+$/) }
+  it { is_expected.to match(/^\d+\.\d+\.\d+(\.\w+(\.\d+)?)?$/) }
 end
