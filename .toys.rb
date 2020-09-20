dc00bbd86010c66604161eb585e65a05c1667545
Alexander Popov <alex.wayfer@gmail.com>
2020-09-20 23:57:24 +0300

    Replace custom `rspec` and `rubocop` toys tools with built-in ones

--- .toys.rb
+++ .toys.rb
@@ -2,30 +2,14 @@
 
 include :bundler, static: true
 
-subtool_apply do
-  include :exec, exit_on_nonzero_status: true, log_level: Logger::UNKNOWN unless include? :exec
-end
-
 require 'gem_toys'
 expand GemToys::Template
 
 alias_tool :g, :gem
 
-tool :rspec do
-  disable_argument_parsing
+expand :rspec
 
-  def run
-    exec ['rspec', *args]
-  end
-end
+alias_tool :rspec, :spec
+alias_tool :test, :spec
 
-alias_tool :spec, :rspec
-alias_tool :test, :rspec
-
-tool :rubocop do
-  disable_argument_parsing
-
-  def run
-    exec ['rubocop', *args]
-  end
-end
+expand :rubocop
