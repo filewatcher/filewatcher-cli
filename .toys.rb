fcd4ccdd5dc02952ff742dad49453c379501335f
Alexander Popov <alex.wayfer@gmail.com>
2021-02-23 01:42:25 +0300

    Add `toys console` tool (with `c` alias)

--- .toys.rb
+++ .toys.rb
@@ -6,3 +6,14 @@ require 'gem_toys'
 expand GemToys::Template, version_file_path: "#{__dir__}/lib/filewatcher/cli/constants.rb"
 
 alias_tool :g, :gem
+
+tool :console do
+  def run
+    require_relative 'lib/filewatcher/cli'
+
+    require 'pry'
+    Pry.start
+  end
+end
+
+alias_tool :c, :console
