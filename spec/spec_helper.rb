42d4e80d4bd00348a49cc3167060994561f2219f
Alexander Popov <alex.wayfer@gmail.com>
2022-09-08 18:40:13 +0300

    Enable RSpec `--only-fail` feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,9 @@ if ENV['CODECOV_TOKEN']
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
+RSpec.configure do |c|
+  c.example_status_persistence_file_path = "#{__dir__}/examples.txt"
+end
+
 require_relative '../lib/filewatcher/cli'
 require_relative '../lib/filewatcher/cli/spec_helper'
