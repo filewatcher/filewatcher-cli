298f03d6a029ec7d8ead965ee2cb01bea8fff2b3
Alexander Popov <alex.wayfer@gmail.com>
2022-11-17 22:20:43 +0300

    Add specs for paths splitted by spaces
    Return old funcionallity.

--- lib/filewatcher/cli/spec_helper/dump_to_file.rb
+++ lib/filewatcher/cli/spec_helper/dump_to_file.rb
@@ -4,5 +4,10 @@ require_relative 'shell_watch_run'
 
 def dump_to_file(content)
   Filewatcher::CLI::SpecHelper.debug "#{__method__} #{content.inspect}"
-  File.write File.join(Filewatcher::CLI::SpecHelper::ShellWatchRun::DUMP_FILE), content
+
+  File.write(
+    File.join(Filewatcher::CLI::SpecHelper::ShellWatchRun::DUMP_FILE),
+    "#{content}\n",
+    mode: 'a+'
+  )
 end
